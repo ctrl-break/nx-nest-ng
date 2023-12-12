86ac193294e1a40de18834bad1f4d8878e4c49d5
Alexander Bashenin <aobashenin@gmail.com>
2023-12-12 09:37:46 +0300

    added nest +eslint airbnb/base, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: 'tsconfig.base.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint/eslint-plugin'],
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'airbnb-typescript/base',
+        'plugin:import/recommended',
+        'plugin:prettier/recommended',
+    ],
+    root: true,
+    env: {
+        node: true,
+        jest: true,
+    },
+    ignorePatterns: ['.eslintrc.js'],
+    rules: {},
+};
