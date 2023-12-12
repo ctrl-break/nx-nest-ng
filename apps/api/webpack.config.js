34650c773576a881d5e6504f34060dd848d30dd8
Alexander Bashenin <aobashenin@gmail.com>
2023-12-12 09:56:40 +0300

    swc builder for api

--- apps/api/webpack.config.js
+++ apps/api/webpack.config.js
@@ -1,13 +1,16 @@
-const { composePlugins, withNx } = require('@nx/webpack');
+const swcDefaultConfig = require('@nestjs/cli/lib/compiler/defaults/swc-defaults').swcDefaultsFactory().swcOptions;
 
-// Nx plugins for webpack.
-module.exports = composePlugins(
-  withNx({
-    target: 'node',
-  }),
-  (config) => {
-    // Update the webpack config as needed here.
-    // e.g. `config.plugins.push(new MyPlugin())`
-    return config;
-  }
-);
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'swc-loader',
+                    options: swcDefaultConfig,
+                },
+            },
+        ],
+    },
+};
