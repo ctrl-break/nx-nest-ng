6fcba85154c01efe190506729bae8ffed65de25b
Alexander Bashenin <timebender00@gmail.com>
2023-12-13 06:18:54 +0300

    [client] added tilewind

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,25 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        project: 'tsconfig.base.json',
-        tsconfigRootDir: __dirname,
-        sourceType: 'module',
-    },
-    plugins: ['@nx', '@typescript-eslint/eslint-plugin'],
-    extends: [
-        'plugin:@typescript-eslint/recommended',
-        'airbnb-typescript/base',
-        'plugin:import/recommended',
-        'plugin:prettier/recommended',
-    ],
-    root: true,
-    env: {
-        node: true,
-        jest: true,
-    },
-    ignorePatterns: ['.eslintrc.js', 'dist/**'],
-    rules: {
-        'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-        'import/named': 'off',
-    },
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: 'tsconfig.base.json',
+		tsconfigRootDir: __dirname,
+		sourceType: 'module',
+	},
+	plugins: ['@nx', '@typescript-eslint/eslint-plugin'],
+	extends: [
+		'plugin:@typescript-eslint/recommended',
+		'airbnb-typescript/base',
+		'plugin:import/recommended',
+		'plugin:prettier/recommended',
+	],
+	root: true,
+	env: {
+		node: true,
+		jest: true,
+	},
+	ignorePatterns: ['.eslintrc.js', 'dist/**', '*.js'],
+	rules: {
+		'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+		'import/named': 'off',
+	},
 };
