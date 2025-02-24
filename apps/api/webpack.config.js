51a14e2ff9f9c514b9eddd2f81cb90a0a1bcf5e4
Alexander Bashenin <timebender00@gmail.com>
2025-02-24 12:50:32 +0400

    updated apps

--- apps/api/webpack.config.js
+++ apps/api/webpack.config.js
@@ -1,32 +1,20 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const swcDefaultConfig = require('@nestjs/cli/lib/compiler/defaults/swc-defaults').swcDefaultsFactory().swcOptions;
+const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
+const { join } = require('path');
 
 module.exports = {
-	module: {
-		rules: [
-			{
-				test: /\.ts$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'swc-loader',
-					options: swcDefaultConfig,
-				},
-			},
-		],
-	},
+  output: {
+    path: join(__dirname, '../../dist/apps/api'),
+  },
+  plugins: [
+    new NxAppWebpackPlugin({
+      target: 'node',
+      compiler: 'tsc',
+      main: './src/main.ts',
+      tsConfig: './tsconfig.app.json',
+      assets: ['./src/assets'],
+      optimization: false,
+      outputHashing: 'none',
+      generatePackageJson: true,
+    }),
+  ],
 };
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-// const { composePlugins, withNx } = require('@nx/webpack');
-
-// // Nx plugins for webpack.
-// module.exports = composePlugins(
-// 	withNx({
-// 		target: 'node',
-// 	}),
-// 	(config) => {
-// 		// Update the webpack config as needed here.
-// 		// e.g. `config.plugins.push(new MyPlugin())`
-// 		return config;
-// 	},
-// );
