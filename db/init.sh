7c36560ad45fdcf3d8441c2445774770e8b6c5b3
Alexander Bashenin <timebender00@gmail.com>
2025-02-24 12:51:56 +0400

    updated init script

--- db/init.sh
+++ db/init.sh
@@ -8,6 +8,8 @@ if [ -f ".env" ]; then
     exit 1
 fi
 
+echo "Enter app name (default: app) "
+read appName
 echo "Enter db name (default: docker) "
 read name
 echo "Enter db username (default: docker) "
@@ -18,12 +20,19 @@ read pass
 stty echo
 
 touch .env
+app=$([[ "$appName" != "" ]] && echo "$appName" || echo "app")
 dbname=$([[ "$name" != "" ]] && echo "$name" || echo "docker")
 dbuser=$([[ "$user" != "" ]] && echo "$user" || echo "docker")
+port="8100"
+uuid=$(cat /proc/sys/kernel/random/uuid)
 
+echo "APP_NAME=$app" >> .env
 echo "DB_DATABASE=$dbname" >> .env
 echo "DB_USERNAME=$dbuser" >> .env
 echo "DB_PASSWORD=$pass" >> .env
+echo "DATABASE_PORT=$port" >> .env
+echo "DATABASE_URL=\"postgresql://$dbuser:$pass@localhost:$port/$dbname\"" >> .env
+echo "JWT_SECRET=$uuid" >> .env
 
 cp ./db/initial.sql ./db/createdb.sql
 filename="./db/createdb.sql"
